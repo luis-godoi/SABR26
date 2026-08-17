8ff8442270dbbe460ff8dbaddceae269dd131ce3
Luis Godoi <gustavo8godoy@gmail.com>
2026-08-17 20:39:49 -0300

    Consertando imports

--- ferramentas.py
+++ ferramentas.py
@@ -3,13 +3,13 @@ import numpy as np
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from langchain.tools import tool
-from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
-from langchain.agents import Tool
+from langchain_core.tools import tool, Tool
 from langchain_experimental.tools import PythonAstREPLTool
 
 # Obtenção da chave de api
